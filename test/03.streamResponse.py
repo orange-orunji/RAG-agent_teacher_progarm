27c2ee592697fb8edb99247ccaea2a5a660638e0
orange-orunji <3078045959@qq.com>
2026-05-29 15:45:08 +0800

    stream流式输出案例

---
+++ test/03.streamResponse.py
@@ -0,0 +1,22 @@
+import sys
+from openai import OpenAI
+
+sys.stdout.reconfigure(encoding="utf-8")
+# 1.获取client客户端对象,OenAI对象
+client : OpenAI = OpenAI(
+  api_key="sk-1a60e37ca4434f41ba71d8e11d0b4e60",
+  base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
+)
+# 2.创建模型
+c = client.chat.completions.create(
+  model="qwen-plus",
+  messages=[
+    {'role':"system",'content':'你是一名资深的java专家'},
+    {'role':"assistant",'content':"好的,我是一名资深的java专家,回答不说废话"},
+    {'role':"user",'content':"redis是什么"}
+  ],
+  stream=True
+)
+# 3.输出返回结果
+for chunk in c:
+  print(chunk.choices[0].delta.content,end="",flush=True)
