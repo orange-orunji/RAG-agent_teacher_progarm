7d31cf1b8cb7d5a72d427eea35338a0bda6993cf
orange-orunji <3078045959@qq.com>
2026-06-01 23:29:41 +0800

    langchain中chain是将内容拼接成一个RunnableSequence

--- RAG_LangChain_demo/13_langchian_chain_using.py
+++ RAG_LangChain_demo/13_langchian_chain_using.py
@@ -10,3 +10,4 @@ print(chain,type(chain))
 
 # first=PromptTemplate(input_variables=['gender', 'lastname'], input_types={}, partial_variables={}, template='我的邻居姓{lastname},是一个{gender},请帮他起个名字') middle=[] last=Tongyi(client=<class 'dashscope.aigc.generation.Generation'>, model_name='qwen-max', model_kwargs={})
 # <class 'langchain_core.runnables.base.RunnableSequence'>
+ 
